7ad6638db4494645830c281e32714cff64b5bfc4
kk201811632210 <97880206+kk201811632210@users.noreply.github.com>
2022-01-17 17:49:49 +0800

    Add files via upload

---
+++ test_1_17.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*这个代码是测试代码，输出hello world代码*/
+//int main()
+//{
+//	printf("hello world\n");
+//	
+//	return 0;
+//}
+
+
+/*这个代码是判断输入的数字是否为奇数，如果是奇数
+则打印出此奇数，如果不是奇数，则将1-100的奇数打印出来*/
+int main()
+{
+	int a;
+	int b = 0;
+	scanf("%d", &a);
+	if (0 != a % 2)
+		printf("%d\n", a);
+	else
+	{
+		printf("此数不为奇数\n打印的奇数为\n");
+		while (b <= 100)
+		{
+			if (0 != b % 2)
+				printf("%d\n", b);
+			b = b++;
+		}
+	}
+
+	return 0;
+}
